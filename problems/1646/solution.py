8a92974d7a2b2136bd75ed1f012bf56e34ac13c1
mys1024 <mys.1024@outlook.com>
2022-01-30 21:19:12 +0800

    solve 1646. 获取生成数组中的最大值

---
+++ problems/1646/solution.py
@@ -0,0 +1,15 @@
+# 1646. 获取生成数组中的最大值
+# URL：https://leetcode-cn.com/problems/get-maximum-in-generated-array/
+# 执行结果：通过
+# 执行用时：36 ms, 在所有 Python3 提交中击败了 37.47% 的用户
+# 内存消耗：14.9 MB, 在所有 Python3 提交中击败了 69.18% 的用户
+
+class Solution:
+  def getMaximumGenerated(self, n: int) -> int:
+    if n < 2:
+      return n
+    nums = [0, 1] + [None] * (n - 1)
+    for i in range(2, n + 1):
+      mid = i // 2
+      nums[i] = nums[mid] + (0 if i % 2 == 0 else nums[mid + 1])
+    return max(nums)
