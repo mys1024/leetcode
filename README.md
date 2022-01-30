2c49b314c5c2250f7ecd2e540776a07e44f09b85
mys1024 <mys.1024@outlook.com>
2022-01-30 20:34:46 +0800

    git init

---
+++ README.md
@@ -0,0 +1,1 @@
+我的 [Leetcode](https://leetcode-cn.com/) 解题记录。
---
+++ problems/884/solution.py
@@ -0,0 +1,23 @@
+# 884. 两句话中的不常见单词
+# URL：https://leetcode-cn.com/problems/uncommon-words-from-two-sentences/
+# 执行结果：通过
+# 执行用时：32 ms, 在所有 Python3 提交中击败了 75.00% 的用户
+# 内存消耗：15 MB, 在所有 Python3 提交中击败了 66.98% 的用户
+
+
+from typing import List
+
+class Solution:
+  def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+    counter = {}
+    words = s1.split(' ') + s2.split(' ')
+    for word in words:
+      if word in counter:
+        counter[word] += 1
+      else:
+        counter.setdefault(word, 1)
+    result = []
+    for (word, count) in counter.items():
+      if count == 1:
+        result.append(word)
+    return result
