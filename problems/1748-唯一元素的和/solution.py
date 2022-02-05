9690604ce718caf64450812251cf57f4aa1e1260
mys1024 <mys.1024@outlook.com>
2022-02-06 02:00:49 +0800

    update 1748. 唯一元素的和

--- problems/1748-唯一元素的和/solution.py
+++ problems/1748-唯一元素的和/solution.py
@@ -2,21 +2,13 @@
 # URL：https://leetcode-cn.com/problems/sum-of-unique-elements/
 # 难度：简单
 # 关键词：数组
-# 执行用时：28 ms, 在所有 Python3 提交中击败了 91.79% 的用户
-# 内存消耗：14.9 MB, 在所有 Python3 提交中击败了 84.33% 的用户
+# 执行用时：24 ms, 在所有 Python3 提交中击败了 98.01% 的用户
+# 内存消耗：14.8 MB, 在所有 Python3 提交中击败了 89.80% 的用户
 
-from typing import List, Dict
+from collections import Counter
+from typing import List
 
 class Solution:
   def sumOfUnique(self, nums: List[int]) -> int:
-    flags: Dict[int, bool] = {}
-    s = 0
-    for num in nums:
-      try:
-        if flags[num]:
-          s -= num
-          flags[num] = False
-      except:
-        flags[num] = True
-        s += num
-    return s
+    counter = Counter(nums)
+    return sum(map(lambda i: i[0], filter(lambda i: i[1] == 1, counter.items())))
