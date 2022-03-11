2455dfb22e711b30171e2b5c2ea10cca59e5fed9
mys1024 <mys.1024@outlook.com>
2022-03-11 20:51:33 +0800

    update 6018. 根据描述创建二叉树

--- problems/6018.create-binary-tree-from-descriptions/solution.go
+++ problems/6018.create-binary-tree-from-descriptions/solution.go
@@ -2,8 +2,8 @@
 // URL：https://leetcode-cn.com/problems/create-binary-tree-from-descriptions/
 // 难度：中等
 // 关键词：二叉树
-// 执行用时：312 ms, 在所有 Go 提交中击败了 100.00% 的用户
-// 内存消耗：8.1 MB, 在所有 Go 提交中击败了 100.00% 的用户
+// 执行用时：296 ms, 在所有 Go 提交中击败了 75.76% 的用户
+// 内存消耗：8.1 MB, 在所有 Go 提交中击败了 87.37% 的用户
 
 package main
 
@@ -13,53 +13,30 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-type IntSet struct {
-	m map[int]bool
-}
-
-func NewIntSet() *IntSet {
-	return &IntSet{m: map[int]bool{}}
-}
-
-func (s *IntSet) Add(num int) {
-	s.m[num] = true
-}
-
-func (s *IntSet) Remove(num int) {
-	delete(s.m, num)
-}
-
-func (s *IntSet) ToSlice() []int {
-	slice := []int{}
-	for n := range s.m {
-		slice = append(slice, n)
-	}
-	return slice
-}
-
 func createBinaryTree(descriptions [][]int) *TreeNode {
 	nodes := map[int]*TreeNode{}
-	rootNodes := NewIntSet()
+	marked := map[int]bool{}
+
 	for _, description := range descriptions {
-		parent, child := description[0], description[1]
+		parent, child, isLeft := description[0], description[1], description[2]
 		if nodes[parent] == nil {
 			nodes[parent] = &TreeNode{Val: parent}
-			rootNodes.Add(parent)
 		}
 		if nodes[child] == nil {
 			nodes[child] = &TreeNode{Val: child}
-			rootNodes.Add(parent)
 		}
-	}
-	for _, description := range descriptions {
-		parent, child, left := description[0], description[1], description[2]
-		rootNodes.Remove(child)
-		if left == 1 {
+		if isLeft == 1 {
 			nodes[parent].Left = nodes[child]
 		} else {
 			nodes[parent].Right = nodes[child]
 		}
+		marked[child] = true
 	}
-	root := rootNodes.ToSlice()[0]
-	return nodes[root]
+
+	for nodeVal, node := range nodes {
+		if !marked[nodeVal] {
+			return node
+		}
+	}
+	return nil
 }
