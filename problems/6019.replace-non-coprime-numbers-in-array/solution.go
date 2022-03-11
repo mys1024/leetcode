09395f739b42b6fbf217d4410edb27dc116db42a
mys1024 <mys.1024@outlook.com>
2022-03-11 22:44:20 +0800

    update 6019. 替换数组中的非互质数

--- problems/6019.replace-non-coprime-numbers-in-array/solution.go
+++ problems/6019.replace-non-coprime-numbers-in-array/solution.go
@@ -1,11 +1,9 @@
 // 6019. 替换数组中的非互质数
 // URL：https://leetcode-cn.com/problems/replace-non-coprime-numbers-in-array/
 // 难度：困难
-// 关键词：数学、数论、最大公约数、最小公倍数、辗转相除法、链表
-// 执行用时：312 ms, 在所有 Go 提交中击败了 100.00% 的用户
-// 内存消耗：8.1 MB, 在所有 Go 提交中击败了 100.00% 的用户
-
-// 此题也可考虑使用栈来解题
+// 关键词：数学、数论、最大公约数、最小公倍数、辗转相除法
+// 执行用时：152 ms, 在所有 Go 提交中击败了 86.78% 的用户
+// 内存消耗：8.8 MB, 在所有 Go 提交中击败了 81.82% 的用户
 
 package main
 
@@ -20,48 +18,22 @@ func lcm(a, b int) int {
 	return a * b / gcd(a, b)
 }
 
-type LinkedListNode struct {
-	Val  int
-	Prev *LinkedListNode
-	Next *LinkedListNode
-}
-
-func (n *LinkedListNode) ToSlice() []int {
-	s := []int{}
-	for n != nil {
-		s = append(s, n.Val)
-		n = n.Next
-	}
-	return s
-}
-
 func replaceNonCoprimes(nums []int) []int {
-	// 使用双链表存储 nums
-	head := &LinkedListNode{Val: nums[0]}
-	node := head
+	// 使用 nums 数组作为栈
+	top := 0
 	for i := 1; i < len(nums); i++ {
-		node.Next = &LinkedListNode{Val: nums[i], Prev: node}
-		node = node.Next
-	}
-	// 在双链表中进行非互质数的替换操作
-	for node := head; node.Next != nil; {
-		next := node.Next
-		if gcd(node.Val, next.Val) > 1 {
-			node.Val = lcm(node.Val, next.Val)
-			node.Next = next.Next
-			next.Next = nil
-			next.Prev = nil
-			next = node.Next
-			if next != nil {
-				next.Prev = node
-			}
-			if node.Prev != nil {
-				node = node.Prev
-			}
-		} else {
-			node = node.Next
+		num := nums[i]
+		if gcd(nums[top], num) == 1 {
+			top++
+			nums[top] = num
+			continue
 		}
+		for top >= 0 && gcd(nums[top], num) > 1 {
+			num = lcm(nums[top], num)
+			top--
+		}
+		top++
+		nums[top] = num
 	}
-	// 返回答案
-	return head.ToSlice()
+	return nums[:top+1]
 }
