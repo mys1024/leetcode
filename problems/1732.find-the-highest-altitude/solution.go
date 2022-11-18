ffc7c34f2e3c707810c0920c8b6f10b6aa7d7b8c
mys1024 <mys.1024@outlook.com>
2022-11-19 01:11:03 +0800

    fix 1725. 可以形成最大正方形的矩形数目

--- problems/1732.find-the-highest-altitude/solution.go
+++ problems/1732.find-the-highest-altitude/solution.go
@@ -1,5 +1,5 @@
-// 1725. 可以形成最大正方形的矩形数目
-// URL：https://leetcode-cn.com/problems/number-of-rectangles-that-can-form-the-largest-square/
+// 1732. 找到最高海拔
+// URL：https://leetcode.cn/problems/find-the-highest-altitude/
 // 难度：简单
 // 关键词：数组
 // 执行用时：0 ms, 在所有 Python3 提交中击败了 100.00% 的用户
